87f5ae0a072072ac5caff5e07f43bbe01ab0e6cc
Thomas Kaalaas <thomaskaalaas@Thomass-MacBook-Air.local>
2023-02-13 21:48:04 +0100

    Create app using OOD

---
+++ convertOOD.js
@@ -0,0 +1,24 @@
+MeasureOfLength = {
+    millimeter:{inches:0.0254},
+    centimeter:{inches:2.54},
+    meter:{inches:25.4},
+    feet:{inches:12}
+}
+
+MeasureOfVolume = {
+    milliliter:{quarts:946.353},
+    centiliter:{quarts:94.6352946},
+    deciliter:{quarts:9.46352946},
+    litre:{quarts:0.946353},
+    pint:{quarts:0.5}
+   
+}
+
+class Converter{
+    
+    constructor(){
+        this. millimeterToInches =0.0254;
+        this. centimeterToInches =2.54;
+        this. meterToInches =25.4;
+        this. feetToInches =12;
+    }
